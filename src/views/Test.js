facf3a2c8dbc7a2085672c0be55240afd61413ed
dd <dd@dddeMacBook-Pro.local>
2016-08-09 17:33:41 +0800

    frst

--- src/views/Test.js
+++ src/views/Test.js
@@ -25,7 +25,7 @@ class Test extends Component {
   }
 
   render() {
-    return (<div onTouchTap={::this.goTo}>Hello World!111</div>);
+    return (<div onTouchTap={::this.goTo}>Hello World!</div>);
   }
 
   componentDidMount() {
